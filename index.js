6c01fee55b189e2ff7fd2d2ed84e744a7962ca65
OhBeomho <tyler0322@naver.com>
2023-09-20 18:58:06 +0900

    TODO: code checkWin()

--- index.js
+++ index.js
@@ -14,22 +14,44 @@ class Room {
 	constructor(name) {
 		this.name = name;
 		this.users = [];
+		this.board = [];
 		this.turn = 0;
+
+		// 15x15
+		for (let i = 0; i < 15; i++) {
+			this.board.push(new Array(15));
+		}
 	}
 
 	place(x, y) {
-		// TODO: Place
-
+		this.board[y][x] = this.turn;
+		this.turn = this.turn === 1 ? 0 : 1;
+		wsServer.to(this.users[this.turn]).emit("place", x, y);
 		this.checkWin();
 	}
 
 	checkWin() {
-		// TODO: Check
+		let winner;
+		for (let y = 0; y < 15; y++) {
+			const line = this.board[y];
 
-		// If someone wins
-		let winner = "Change Here!!";
-		wsServer.to(this.name).emit("win", winner);
-		rooms.splice(rooms.indexOf(this), 1);
+			if (line.filter((v) => typeof v === "number").length >= 5) {
+				for (let x = 0; x < 10; x++) {
+					if (line.slice(x, x + 5).every((v) => v)) {
+						winner = users[line[x]];
+					}
+				}
+			}
+		}
+
+		// TODO: 세로, 대각선 확인
+
+		if (winner) {
+			wsServer.to(this.name).emit("win", winner);
+			rooms.splice(rooms.indexOf(this), 1);
+		} else if (!winner && !this.board.find((v) => typeof v !== "number")) {
+			wsServer.to(this.name).emit("win", -1);
+		}
 	}
 }
 
@@ -41,19 +63,55 @@ wsServer.on("connection", (socket) => {
 	socket.once("room", (roomName) => {
 		const targetRoom = rooms.find((r) => r.name === roomName);
 		if (!targetRoom) {
-			socket.disconnect();
+			socket.emit("room", {
+				type: "error",
+				message: "참가하려는 방은 존재하지 않는 방입니다."
+			});
+		} else if (targetRoom.users.length === 2) {
+			socket.emit("room", {
+				type: "error",
+				message: "참가하려는 방은 이미 게임이 진행중입니다."
+			});
+		} else {
+			room = targetRoom;
+			room.users.push(socket.id);
+			socket.join(roomName);
+
+			if (room.users.length === 2) {
+				socket.emit("room", { type: "start" });
+			} else {
+				socket.emit("room", { type: "success" });
+			}
+
 			return;
 		}
 
-		room = targetRoom;
-		room.users.push(this.socket.id);
-		socket.join(roomName);
+		socket.disconnect();
 	});
-	socket.on("place", (x, y) => {});
+	socket.on("start", () => {
+		if (!room || room.users.length < 2) {
+			return;
+		}
+
+		startGame();
+		socket.removeAllListeners("start");
+	});
+
+	const startGame = () => {
+		socket.on("place", room.place);
+		socket.on("disconnect", () => {
+			socket.emit(
+				"win",
+				room.users.find((v) => v !== socket.id)
+			);
+			rooms.splice(rooms.indexOf(this), 1);
+		});
+	};
 });
 
 app.use("/", express.static(path.join(__dirname, "pages")));
-app.use("/styles", express.static(path.join(__dirname, "styles")));
+app.use("/styles", express.static(path.join(__dirname, "css")));
+app.use("/scripts", express.static(path.join(__dirname, "js")));
 
 app.get("/room_list", (req, res) => res.json(rooms));
 app.get("/create_room/:roomName", (req, res) => {
