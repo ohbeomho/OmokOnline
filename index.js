20a8080c2e6eff18dfea21989f063f58fa283d33
OhBeomho <tyler0322@naver.com>
2023-09-28 11:04:16 +0900

    TODO: Make game table

--- index.js
+++ index.js
@@ -17,68 +17,51 @@ class Room {
 		this.turn = 0;
 
 		// 15x15
-		for (let i = 0; i < 15; i++) {
-			this.board[i] = new Array(15).fill(undefined);
-		}
+		for (let i = 0; i < 15; i++) this.board[i] = new Array(15).fill(undefined);
 	}
 
-	place(x, y) {
+	place(x, y, id) {
+		if (this.users.indexOf(id) !== this.turn) return;
+
 		this.board[y][x] = this.turn;
+		wsServer.to(this.users).emit("game", { type: "place", x, y, turn: this.turn });
 		this.turn = this.turn === 1 ? 0 : 1;
-		wsServer.to(this.users[this.turn]).emit("place", x, y);
 		this.checkWin();
 	}
 
 	checkWin() {
-		let winner;
+		const win = (winner) => {
+			wsServer.to(this.name).emit("game", { type: "win", winner });
+			rooms.splice(rooms.indexOf(this), 1);
+		};
 
 		for (let y = 0; y < 15; y++) {
 			const line = this.board[y];
 
 			for (let x = 0; x <= 10; x++) {
 				const five = line.slice(x, x + 5);
-
-				if (five.every((v) => typeof v === "number" && v === five[0])) {
-					winner = users[five[0]];
-				}
+				if (five.every((v) => typeof v === "number" && v === five[0])) win(users[five[0]]);
 			}
 		}
 
 		for (let x = 0; x < 15; x++) {
 			const line = [];
-			for (let y = 0; y < 15; y++) {
-				line.push(this.board[y][x]);
-			}
-
+			for (let y = 0; y < 15; y++) line.push(this.board[y][x]);
 			for (let y = 0; y <= 10; y++) {
 				const five = line.slice(y, y + 5);
-
-				if (five.every((v) => typeof v === "number" && v === five[0])) {
-					winner = users[five[0]];
-				}
+				if (five.every((v) => typeof v === "number" && v === five[0])) win(users[five[0]]);
 			}
 		}
 
 		for (let y = 0; y <= 10; y++) {
 			for (let x = 0; x <= 10; x++) {
 				const five = [];
-				for (let i = 0; i < 5; i++) {
-					five.push(this.board[y + i][x + i]);
-				}
-
-				if (five.every((v) => typeof v === "number" && v === five[0])) {
-					winner = users[five[0]];
-				}
+				for (let i = 0; i < 5; i++) five.push(this.board[y + i][x + i]);
+				if (five.every((v) => typeof v === "number" && v === five[0])) win(users[five[0]]);
 			}
 		}
 
-		if (winner) {
-			wsServer.to(this.name).emit("win", winner);
-			rooms.splice(rooms.indexOf(this), 1);
-		} else if (!winner && !this.board.find((v) => typeof v !== "number")) {
-			wsServer.to(this.name).emit("win", -1);
-			rooms.splice(rooms.indexOf(this), 1);
-		}
+		if (!this.board.find((v) => typeof v !== "number")) win(undefined);
 	}
 }
 
@@ -106,7 +89,13 @@ wsServer.on("connection", (socket) => {
 
 			if (room.users.length === 2) {
 				room.users.forEach((u, i) =>
-					wsServer.to(u).emit("room", { type: "start", turn: i })
+					wsServer
+						.to(u)
+						.emit("room", {
+							type: "start",
+							turn: i,
+							opponent: room.users[i === 0 ? 1 : 0]
+						})
 				);
 			} else {
 				socket.emit("room", { type: "success" });
@@ -127,12 +116,12 @@ wsServer.on("connection", (socket) => {
 	});
 
 	const startGame = () => {
-		socket.on("place", room.place);
+		socket.on("game", (x, y) => room.place(x, y, socket.id));
 		socket.on("disconnect", () => {
-			socket.emit(
-				"win",
-				room.users.find((v) => v !== socket.id)
-			);
+			socket.emit("game", {
+				type: "win",
+				winner: room.users.find((v) => v !== socket.id)
+			});
 			rooms.splice(rooms.indexOf(this), 1);
 		});
 	};
--- js/game.js
+++ js/game.js
@@ -1,7 +1,61 @@
 const socket = io();
 const roomName = decodeURIComponent(new URLSearchParams(location.search).get("room"));
+const message = document.querySelector(".message");
+const black = document.querySelector(".black");
+const white = document.querySelector(".white");
+let myTurn, opponent;
+
+function startGame() {
+	message.innerText = "";
+	const arr = [black, white];
+	arr[myTurn].classList.add("me");
+
+	const idArr = arr.map((e) => e.querySelector(".id"));
+	if (myTurn === 0) {
+		idArr[0].innerText = socket.id;
+		idArr[1].innerText = opponent;
+	} else {
+		idArr[1].innerText = socket.id;
+		idArr[0].innerText = opponent;
+	}
+
+	socket.on("game", (data) => {
+		switch (data.type) {
+			case "win":
+				win(data.winner);
+			case "place":
+				const { x, y, turn } = data;
+				place(x, y, turn);
+			default:
+		}
+	});
+}
+
+function win(winner) {
+	if (winner === socket.id) alert("승리!");
+	else if (winner === undefined) alert("무승부!");
+	else alert("패배..");
+
+	location.assign("/");
+}
+
+function place(x, y, turn) {
+	// TODO: table 의 x, y 위치에 표시
+}
 
 socket.on("room", (data) => {
-	// TODO: Make it
+	switch (data.type) {
+		case "error":
+			alert(data.message);
+			location.assign("/");
+			break;
+		case "success":
+			message.innerText = "상대 접속 대기중...";
+			break;
+		case "start":
+			({ myTurn, opponent } = data);
+			startGame();
+		default:
+	}
 });
 socket.emit("room", roomName);
