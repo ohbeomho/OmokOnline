0742d76d582a403e7a2ec1cc3d91e062bf06a215
OhBeomho <tyler0322@naver.com>
2023-09-30 12:37:49 +0900

    새 관전 기능에 맞게 UI 개선, 관전자도 플레이어가 나간 것을 알 수 있음

--- index.js
+++ index.js
@@ -10,84 +10,84 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 class Room {
-	constructor(name) {
-		this.name = name;
-		this.users = [];
-		this.spectators = [];
-		this.board = new Array(15);
-		this.turn = 0;
+    constructor(name) {
+        this.name = name;
+        this.users = [];
+        this.spectators = [];
+        this.board = new Array(15);
+        this.turn = 0;
 
-		// 15x15
-		for (let i = 0; i < 15; i++) this.board[i] = new Array(15).fill(undefined);
-	}
+        // 15x15
+        for (let i = 0; i < 15; i++) this.board[i] = new Array(15).fill(undefined);
+    }
 
-	place(x, y, user) {
-		if (this.users.indexOf(user) !== this.turn || x > 14 || y > 14 || x < 0 || y < 0) return;
+    place(x, y, user) {
+        if (this.users.indexOf(user) !== this.turn || x > 14 || y > 14 || x < 0 || y < 0) return;
 
-		this.board[y][x] = this.turn;
-		io.to(this.name).emit("game", {
-			type: "place",
-			x,
-			y,
-			turn: this.turn
-		});
-		this.turn = this.turn === 1 ? 0 : 1;
-		this.checkWin();
-	}
+        this.board[y][x] = this.turn;
+        io.to(this.name).emit("game", {
+            type: "place",
+            x,
+            y,
+            turn: this.turn
+        });
+        this.turn = this.turn === 1 ? 0 : 1;
+        this.checkWin();
+    }
 
-	checkWin() {
-		const win = (winner, highlight) => {
-			io.to(this.name).emit("game", { type: "win", winner, highlight });
-			rooms.splice(rooms.indexOf(this), 1);
-		};
+    checkWin() {
+        const win = (winner, highlight) => {
+            io.to(this.name).emit("game", { type: "win", winner, highlight });
+            rooms.splice(rooms.indexOf(this), 1);
+        };
 
-		for (let y = 0; y < 15; y++) {
-			const line = this.board[y];
+        for (let y = 0; y < 15; y++) {
+            const line = this.board[y];
 
-			for (let x = 0; x <= 10; x++) {
-				const five = line.slice(x, x + 5);
-				if (five.every((v) => typeof v === "number" && v === five[0]))
-					win(this.users[five[0]], { type: "h", x, y });
-			}
-		}
+            for (let x = 0; x <= 10; x++) {
+                const five = line.slice(x, x + 5);
+                if (five.every((v) => typeof v === "number" && v === five[0]))
+                    win(this.users[five[0]], { type: "h", x, y });
+            }
+        }
 
-		for (let x = 0; x < 15; x++) {
-			const line = [];
-			for (let y = 0; y < 15; y++) line.push(this.board[y][x]);
-			for (let y = 0; y <= 10; y++) {
-				const five = line.slice(y, y + 5);
-				if (five.every((v) => typeof v === "number" && v === five[0]))
-					win(this.users[five[0]], { type: "v", x, y });
-			}
-		}
+        for (let x = 0; x < 15; x++) {
+            const line = [];
+            for (let y = 0; y < 15; y++) line.push(this.board[y][x]);
+            for (let y = 0; y <= 10; y++) {
+                const five = line.slice(y, y + 5);
+                if (five.every((v) => typeof v === "number" && v === five[0]))
+                    win(this.users[five[0]], { type: "v", x, y });
+            }
+        }
 
-		for (let y = 0; y <= 10; y++) {
-			for (let x = 0; x <= 10; x++) {
-				const five = [];
-				for (let i = 0; i < 5; i++) five.push(this.board[y + i][x + i]);
-				if (five.every((v) => typeof v === "number" && v === five[0]))
-					win(this.users[five[0]], { type: "d", x, y });
-			}
-		}
+        for (let y = 0; y <= 10; y++) {
+            for (let x = 0; x <= 10; x++) {
+                const five = [];
+                for (let i = 0; i < 5; i++) five.push(this.board[y + i][x + i]);
+                if (five.every((v) => typeof v === "number" && v === five[0]))
+                    win(this.users[five[0]], { type: "d", x, y });
+            }
+        }
 
-		for (let y = 0; y <= 10; y++) {
-			for (let x = 0; x <= 10; x++) {
-				const five = [];
-				for (let i = 0; i < 5; i++) five.push(this.board[y + i][x + i]);
-				if (five.every((v) => typeof v === "number" && v === five[0]))
-					win(this.users[five[0]], { type: "d", x, y });
-			}
+        for (let y = 0; y <= 10; y++) {
+            for (let x = 0; x <= 10; x++) {
+                const five = [];
+                for (let i = 0; i < 5; i++) five.push(this.board[y + i][x + i]);
+                if (five.every((v) => typeof v === "number" && v === five[0]))
+                    win(this.users[five[0]], { type: "d", x, y });
+            }
 
-			for (let x = 14; x >= 4; x--) {
-				const five = [];
-				for (let i = 0; i < 5; i++) five.push(this.board[y + i][x - i]);
-				if (five.every((v) => typeof v === "number" && v === five[0]))
-					win(this.users[five[0]], { type: "rd", x, y });
-			}
-		}
+            for (let x = 14; x >= 4; x--) {
+                const five = [];
+                for (let i = 0; i < 5; i++) five.push(this.board[y + i][x - i]);
+                if (five.every((v) => typeof v === "number" && v === five[0]))
+                    win(this.users[five[0]], { type: "rd", x, y });
+            }
+        }
 
-		if (!this.board.find((v) => typeof v !== "number")) win(undefined, undefined);
-	}
+        if (!this.board.find((v) => typeof v !== "number")) win(undefined, undefined);
+    }
 }
 
 const rooms = [];
@@ -99,87 +99,88 @@ app.use("/assets", express.static(path.join(__dirname, "assets")));
 
 app.get("/room_list", (req, res) => res.json(rooms));
 app.get("/create_room/:roomName", (req, res) => {
-	const { roomName } = req.params;
+    const { roomName } = req.params;
 
-	if (rooms.find((room) => room.name === roomName)) {
-		res.sendStatus(409);
-		return;
-	}
+    if (rooms.find((room) => room.name === roomName)) {
+        res.sendStatus(409);
+        return;
+    }
 
-	rooms.push(new Room(roomName));
-	res.sendStatus(200);
+    rooms.push(new Room(roomName));
+    res.sendStatus(200);
 });
 
 io.on("connection", (socket) => {
-	let room, user, spec;
+    let room, user, spec;
 
-	socket.once("room", (roomName, username, spectate) => {
-		const targetRoom = rooms.find((r) => r.name === roomName);
-		if (!targetRoom)
-			socket.emit("room", {
-				type: "error",
-				message: "참가하려는 방은 존재하지 않는 방입니다."
-			});
-		else if (targetRoom.users.length === 2 && !spectate)
-			socket.emit("room", {
-				type: "error",
-				message: "참가하려는 방은 이미 게임이 진행중입니다."
-			});
-		else if (targetRoom.users.length === 1 && spectate)
-			socket.emit("room", {
-				type: "error",
-				message: "관전하려는 방은 게임이 시작되지 않았습니다."
-			});
-		else {
-			room = targetRoom;
-			user = { id: socket.id, username };
-			spec = spectate;
-			if (spectate) {
-				room.spectators.push(user);
-				room.users.forEach((user) => io.to(user.id).emit("game", { type: "join-spec" }));
-				socket.emit("room", {
-					type: "room-info",
-					room
-				});
-			} else room.users.push(user);
+    socket.once("room", (roomName, username, spectate) => {
+        const targetRoom = rooms.find((r) => r.name === roomName);
+        if (!targetRoom)
+            socket.emit("room", {
+                type: "error",
+                message: "참가하려는 방은 존재하지 않는 방입니다."
+            });
+        else if (targetRoom.users.length === 2 && !spectate)
+            socket.emit("room", {
+                type: "error",
+                message: "참가하려는 방은 이미 게임이 진행중입니다."
+            });
+        else if (targetRoom.users.length === 1 && spectate)
+            socket.emit("room", {
+                type: "error",
+                message: "관전하려는 방은 게임이 시작되지 않았습니다."
+            });
+        else {
+            room = targetRoom;
+            user = { id: socket.id, username };
+            spec = spectate;
+            if (spectate) {
+                room.spectators.push(user);
+                room.users.forEach((user) => io.to(user.id).emit("game", { type: "join-spec" }));
+                socket.emit("room", {
+                    type: "room-info",
+                    room
+                });
+            } else room.users.push(user);
 
-			socket.join(roomName);
+            socket.join(roomName);
 
-			if (room.users.length === 2)
-				room.users.forEach((u, i) =>
-					io.to(u.id).emit("room", {
-						type: "start",
-						turn: i,
-						opponent: room.users[i === 0 ? 1 : 0]
-					})
-				);
-			else socket.emit("room", { type: "success" });
+            if (room.users.length === 2)
+                room.users.forEach((u, i) =>
+                    io.to(u.id).emit("room", {
+                        type: "start",
+                        turn: i,
+                        opponent: room.users[i === 0 ? 1 : 0]
+                    })
+                );
+            else socket.emit("room", { type: "success" });
 
-			return;
-		}
+            return;
+        }
 
-		socket.disconnect();
-	});
-	socket.on("start", () => {
-		if (!room || room.users.length < 2) return;
+        socket.disconnect();
+    });
+    socket.on("start", () => {
+        if (!room || room.users.length < 2) return;
 
-		startGame();
-		socket.removeAllListeners("start");
-	});
+        startGame();
+        socket.removeAllListeners("start");
+    });
 
-	const startGame = () => socket.on("game", (x, y) => room.place(x, y, user));
+    const startGame = () => socket.on("game", (x, y) => room.place(x, y, user));
 
-	socket.on("disconnect", () => {
-		if (room && room.users.length > 1 && !spec) {
-			const other = room.users.find((v) => v.id !== socket.id);
-			socket.to(other.id).emit("game", { type: "disconnect" });
-			rooms.splice(rooms.indexOf(this), 1);
-		} else if (spec) {
-			room.users.forEach((user) => io.to(user.id).emit("game", { type: "leave-spec" }));
-		}
-	});
+    socket.on("disconnect", () => {
+        if (room) {
+            if (room.users.length > 1 && !spec)
+                socket.to(room.name).emit("game", { type: "disconnect", user });
+
+            rooms.splice(rooms.indexOf(this), 1);
+        } else if (spec) {
+            room.users.forEach((user) => io.to(user.id).emit("game", { type: "leave-spec" }));
+        }
+    });
 });
 
 server.listen(Number(process.env.PORT), () =>
-	console.log("Server is running on port " + process.env.PORT)
+    console.log("Server is running on port " + process.env.PORT)
 );
